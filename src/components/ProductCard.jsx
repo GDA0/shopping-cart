2b6ef55d1f8481b28e067496e63d1ab8b30a943d
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-20 11:46:49 +0000

    Style product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,24 @@
 import PropTypes from "prop-types";
+import "../assets/styles/ProductCard.css";
 
 export function ProductCard({ product }) {
   return (
-    <div className="card h-100">
+    <div className="card h-100 product-card">
       <img className="card-img-top" src={product.image} alt={product.title} />
-      <div className="card-body">
-        <h5 className="card-title">{product.title}</h5>
-        {/* <div className="card-text">{product.description}</div> */}
-        <button className="btn">-</button>
-        <input className="form-control" type="number" />
-        <button className="btn">+</button>
-        <button className="btn">Add To Cart</button>
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title">
+          {product.title} (${product.price})
+        </h5>
+        <div className="mt-auto">
+          <div className="d-flex gap-1 my-3">
+            <button className="btn btn-secondary">-</button>
+            <input className="form-control" type="number" />
+            <button className="btn btn-secondary">+</button>
+          </div>
+          <div className="text-center">
+            <button className="btn btn-light text-center">Add To Cart</button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -19,7 +27,6 @@ export function ProductCard({ product }) {
 ProductCard.propTypes = {
   product: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    // description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
   }).isRequired,
