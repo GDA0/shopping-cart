4fa3545ae589caa2a76cde06f30f84eb3c1c68ac
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-20 08:10:21 +0000

    Work on error page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root } from "./components/routes/Root";
+import { ErrorPage } from "./components/ErrorPage";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
